428501b5c43768ad43633fad4fe5b3826affb8f5
lingdian618 <lijian430421@gmail.com>
2014-09-06 21:36:35 +0800

    "Just another push"

--- push.sh
+++ push.sh
@@ -2,14 +2,14 @@
 
 if [ $# = 0 ];
 then
-	cmd="Just_another_push"
+	cmd="Just another push"
 else
 	cmd=$@
 fi
 
 echo $cmd
 git add -A
-git commit -m \"$cmd\"
+git commit -m "\"$cmd\""
 git pull origin gh-pages
 git push origin gh-pages
 
