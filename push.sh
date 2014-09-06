40ab79d0d84eb6dc4129caf4323ff1cef0ebf00c
lingdian618 <lijian430421@gmail.com>
2014-09-06 20:36:11 +0800

    fuck

--- push.sh
+++ push.sh
@@ -1,6 +1,16 @@
 # !/bin/bash
 
-git add .
+cmd=''
+if [ $# = 0 ];
+then
+	cmd="Just another push"
+else
+	cmd=$*
+fi
+
+echo $cmd
+
+git add -A
 git commit -m $*
 git pull origin gh-pages
 git push origin gh-pages
